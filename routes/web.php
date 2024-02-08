0234de1c5e047e3e5a6d2d2384c8d686dc341e2a
eri-maulana <maulanabouble11@gmail.com>
2024-02-08 08:28:14 +0700

    2. UI User 'login & dashboard'

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+Route::get('/dashboard', [DashboardController::class, 'index']);
