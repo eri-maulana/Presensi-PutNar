936baaf682d6e37b594c156cd6783bbb114d66aa
eri-maulana <maulanabouble11@gmail.com>
2024-02-10 15:10:09 +0700

    6. Menampilkan Peta Lokasi User

--- resources/views/presensi/create.blade.php
+++ resources/views/presensi/create.blade.php
@@ -1,72 +1,95 @@
 @extends('layouts.presensi')
 @section('header')
-   <!-- App Header -->
-   <div class="appHeader bg-primary text-light">
-      <div class="left">
-          <a href="javascript:;" class="headerButton goBack">
-              <ion-icon name="chevron-back-outline"></ion-icon>
-          </a>
-      </div>
-      <div class="pageTitle">Blank Page</div>
-      <div class="right"></div>
-  </div>
-  <!-- * App Header -->
+    <!-- App Header -->
+    <div class="appHeader bg-primary text-light">
+        <div class="left">
+            <a href="javascript:;" class="headerButton goBack">
+                <ion-icon name="chevron-back-outline"></ion-icon>
+            </a>
+        </div>
+        <div class="pageTitle">Blank Page</div>
+        <div class="right"></div>
+    </div>
+    <!-- * App Header -->
 @endsection
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+    integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+    integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
 <style>
-   .webcam-capture, 
-   .webcam-capture video {
-      display: inline-block;
-      width: 100% !important;
-      margin: auto;
-      height: auto !important;
-      border-radius: 15px;
-   }
+    .webcam-capture,
+    .webcam-capture video {
+        display: inline-block;
+        width: 100% !important;
+        margin: auto;
+        height: auto !important;
+        border-radius: 15px;
+    }
+
+    #map {
+        height: 200px;
+    }
 </style>
 @section('content')
-<div class="row" style="margin-top: 70px;">
-   <div class="col">
+    <div class="row" style="margin-top: 70px;">
+        <div class="col">
 
-      <input type="hidden" id="lokasi">
+            <input type="hidden" id="lokasi">
 
-      <div class="webcam-capture">
+            <div class="webcam-capture">
 
-      </div>
-   </div>
-</div>
-<div class="row my-3">
-   <div class="col">
-      <button class="btn btn-primary btn-block">
-         <ion-icon name="camera-outline" ></ion-icon>
-         Absen
-      </button>
-  </div>
-</div>
+            </div>
+        </div>
+    </div>
+    <div class="row mt-3">
+        <div class="col">
+            <button class="btn btn-primary btn-block">
+                <ion-icon name="camera-outline"></ion-icon>
+                Absen
+            </button>
+        </div>
+    </div>
+    <div class="row mt-3">
+        <div class="col">
+            <div id="map"></div>
+        </div>
+    </div>
 @endsection
 
 @push('myscript')
-   <script>
-      Webcam.set({
-         height         : 480,
-         width          : 640,
-         image_format   : 'jpeg',
-         jpeg_quality   : 80
-      })
+    <script>
+        Webcam.set({
+            height: 480,
+            width: 640,
+            image_format: 'jpeg',
+            jpeg_quality: 80
+        })
 
-      Webcam.attach('.webcam-capture')
+        Webcam.attach('.webcam-capture')
 
-      var lokasi = document.getElementById('lokasi');
-      if(navigator.geolocation){
-         navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-      }
+        var lokasi = document.getElementById('lokasi');
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+        }
 
-      function successCallback(position){
-         lokasi.value = position.coords.latitude + ',' + position.coords.longitude;
-      }
+        function successCallback(position) {
+            lokasi.value = position.coords.latitude + ',' + position.coords.longitude;
+            var map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 17);
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(map);
+            var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
+            var circle = L.circle([position.coords.latitude, position.coords.longitude], {
+                color: 'red',
+                fillColor: '#f03',
+                fillOpacity: 0.5,
+                radius: 20
+            }).addTo(map);
+        }
 
-      function errorCallback()
-      {
-         
-      }
+        function errorCallback() {
 
-   </script>
+        }
+    </script>
 @endpush
